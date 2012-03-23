1eb4c08ef7231b5982d06f92a31c0ae7856ef6f6
Tyler Shelton <sheltotj@rose-hulman.edu>
2012-03-23 17:51:53 -0400

    Factors: code 4

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -5,6 +5,10 @@ public class PrimeFactors {
 
 	public static ArrayList<Integer> getPrimeFactors(int i) {
 		ArrayList<Integer> result = new ArrayList<Integer>();
+		if(i%2 == 0){
+			result.add(2);
+			i/=2;
+		}
 		if((i == 2) || (i == 3)) result.add(i);
 		return result;
 	}
